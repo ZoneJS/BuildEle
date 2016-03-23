24e7912d7778cc70ae03484f6bd494e226b3718f
zhangjunshuai <503488574@qq.com>
2016-03-23 09:07:25 +0800

    init

---
+++ BuildingElementsCollecting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuildingElementsCollecting
+//
+//  Created by 张军帅 on 16/3/22.
+//  Copyright © 2016年 zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuildingElementsCollecting/Controllers/JSFMDBVC.h
@@ -0,0 +1,13 @@
+//
+//  JSFMDBVC.h
+//  BuildingElementsCollecting
+//
+//  Created by 张军帅 on 16/3/22.
+//  Copyright © 2016年 zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSFMDBVC : UIViewController
+
+@end
